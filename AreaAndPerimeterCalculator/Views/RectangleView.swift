aadbc6acd5c0eb73a56c84895401c99a3f74e7a9
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-19 11:25:55 -0500

    Created a static layout to show the length, width and area of a rectangle.

--- AreaAndPerimeterCalculator/Views/RectangleView.swift
+++ AreaAndPerimeterCalculator/Views/RectangleView.swift
@@ -9,12 +9,47 @@ import SwiftUI
 
 struct RectangleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            HStack{
+                
+                Image("Rectangle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250)
+                
+                Spacer()
+            }
+            
+            Text("Length")
+                .font(.title2)
+                .bold()
+            
+            Text("7.0")
+                .font(.title2)
+            
+            Text("Width")
+                .font(.title2)
+                .bold()
+            
+            Text("5.0")
+                .font(.title2)
+            
+            Text("Area")
+                .font(.title2)
+                .bold()
+            
+            Text("35.0")
+                .font(.title2)
+            
+            Spacer()
+            
+        }
     }
-}
-
-struct RectangleView_Previews: PreviewProvider {
-    static var previews: some View {
-        RectangleView()
+    
+    struct RectangleView_Previews: PreviewProvider {
+        static var previews: some View {
+            RectangleView()
+        }
     }
 }
