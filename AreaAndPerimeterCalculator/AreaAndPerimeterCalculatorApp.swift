675a18654d7a066938a2c0ca1f7608b9928327d9
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-19 11:04:49 -0500

    Initial Commit

---
+++ AreaAndPerimeterCalculator/AreaAndPerimeterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaAndPerimeterCalculatorApp.swift
+//  AreaAndPerimeterCalculator
+//
+//  Created by dyhidrogen monoxide on 2023/1/19.
+//
+
+import SwiftUI
+
+@main
+struct AreaAndPerimeterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
